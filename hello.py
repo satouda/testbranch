2925d3d5506da623950f75d77cfbc900d80a9cc3
Daisuke Sato <38343374+satouda@users.noreply.github.com>
2018-04-13 16:11:21 +0900

    test 1

---
+++ hello.py
@@ -0,0 +1,40 @@
+#coding:UTF-8
+
+from flask import Flask, render_template #追加
+
+import pymysql #追加
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+
+    #db setting
+    db = pymysql.connect(
+            host='localhost',
+            user='root',
+            password='root',
+            db='testdb',
+            charset='utf8',
+            cursorclass=pymysql.cursors.DictCursor,
+        )
+
+    cur = db.cursor()
+    sql = "select * from members"
+    cur.execute(sql)
+    members = cur.fetchall()
+
+    cur.close()
+    db.close()
+
+    #return name
+    return render_template('hello.html', title='flask test', members=members) #変更
+
+@app.route('/good')
+def good():
+    name = "Good"
+    return name
+
+## おまじない
+if __name__ == "__main__":
+    app.run(debug=True)
